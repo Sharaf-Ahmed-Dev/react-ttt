de1dedcd27f1fd09183831559560f61de6beaeb7
Sharaf-Ahmed-Dev <245864029+Sharaf-Ahmed-Dev@users.noreply.github.com>
2026-01-18 20:34:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Tictactoe from './Components/TICTACTOE/tictactoe';
+
+function App() {
+
+  return (
+    <>
+    <Tictactoe/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/TICTACTOE/tictactoe.jsx
@@ -0,0 +1,136 @@
+import './tictactoe.css';
+import circle from '../Assets/circle.png';
+import x from '../Assets/x.png';
+import { useRef, useState } from 'react';
+
+let data = ["", "", "", "", "", "", "", "", ""];
+
+
+function Tictactoe() {
+
+  let [count, setCount] = useState(0);
+  let [lock, setLock] = useState(false);
+  let headingRef = useRef(null);
+  let box1 = useRef(null);
+  let box2 = useRef(null);
+  let box3 = useRef(null);
+  let box4 = useRef(null);
+  let box5 = useRef(null);
+  let box6 = useRef(null);
+  let box7 = useRef(null);
+  let box8 = useRef(null);
+  let box9 = useRef(null);
+  let box_array = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
+
+  // Click a box tranform into cross and x icon function
+  function toggle(e, num) {
+    if (lock || data[num] !== "") {
+      return;
+    }
+    let newCount = count + 1;
+
+    if (count % 2 === 0) {
+      e.target.innerHTML = `<img src='${x}'/>`;
+      data[num] = "x";
+    } else {
+      e.target.innerHTML = `<img src='${circle}'/>`;
+      data[num] = "o";
+    }
+    setCount(newCount);
+    checkWin(newCount);
+  }
+
+
+  // check winning possibilities 
+  function checkWin(count) {
+
+    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data[2]);
+    }
+    else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
+      won(data[5]);
+    }
+    else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
+      won(data[8]);
+    }
+    else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
+      won(data[6]);
+    }
+    else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
+      won(data[7]);
+    }
+    else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
+      won(data[8]);
+    }
+    else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
+      won(data[6]);
+    }
+    else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
+      won(data[8]);
+    }
+    else if (count === 9 && !lock) {
+      draw();
+    }
+
+  }
+
+
+  // Handles match draw condition
+  function draw() {
+    setLock(true);
+    headingRef.current.innerHTML = "The match ended in a draw!";
+
+  }
+
+
+  // winner showing function in heading section
+  function won(winner) {
+    setLock(true);
+    if (winner === "x") {
+      headingRef.current.innerHTML = `Congratulations! Player <img src = ${x}> wins.`
+    }
+    else {
+      headingRef.current.innerHTML = `Congratulations! Player <img src = ${circle}> wins.`
+    }
+  }
+
+  // game restart function (new game)
+  function reset() {
+    setLock(false);
+    setCount(0);
+    data = ["", "", "", "", "", "", "", "", ""];
+    headingRef.current.innerHTML = "Tic Tac Toe";
+
+    box_array.forEach((e) => {
+      e.current.innerHTML = "";
+    });
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="heading" ref={headingRef}>Tic Tac Toe</h1>
+        <div className="board">
+          <div className="row1">
+            <div className="boxes" ref={box1} onClick={(e) => { toggle(e, 0) }}></div>
+            <div className="boxes" ref={box2} onClick={(e) => { toggle(e, 1) }} ></div>
+            <div className="boxes" ref={box3} onClick={(e) => { toggle(e, 2) }}></div>
+          </div>
+          <div className="row2">
+            <div className="boxes" ref={box4} onClick={(e) => { toggle(e, 3) }} ></div>
+            <div className="boxes" ref={box5} onClick={(e) => { toggle(e, 4) }} ></div>
+            <div className="boxes" ref={box6} onClick={(e) => { toggle(e, 5) }} ></div>
+          </div>
+          <div className="row2">
+            <div className="boxes" ref={box7} onClick={(e) => { toggle(e, 6) }}></div>
+            <div className="boxes" ref={box8} onClick={(e) => { toggle(e, 7) }}></div>
+            <div className="boxes" ref={box9} onClick={(e) => { toggle(e, 8) }}></div>
+          </div>
+        </div>
+        <button className='newgame' onClick={() => { reset() }}>New Game</button>
+      </div>
+    </>
+  )
+};
+
+export default Tictactoe;
